2fcc5df3880db1b44d57f64e28278fe8fe7033ef
mac <mac@ilham.mshome.net>
2025-11-20 18:05:14 +0100

    first attempt

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const base = process.env.VITE_BASE_PATH || '/';
     return {
       server: {
         port: 3000,
